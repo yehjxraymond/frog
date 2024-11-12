33535951e9db03c3cbd55e67cc35f3f9c2b44fd4
Raymond Yeh <ray@geek.sg>
2024-11-12 13:28:15 +0700

    fix: regex

--- src/app/postFrog.ts
+++ src/app/postFrog.ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 export async function postFrog(url: string) {
   // Validate the URL shape
   const regex =
-    /^http:\/\/dc7\.getfrogs\.xyz\/necklace\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+    /^https?:\/\/dc7\.getfrogs\.xyz\/necklace\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
   if (!regex.test(url)) {
     throw new Error("Invalid URL format");
   }
